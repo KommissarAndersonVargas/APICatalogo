50e2f9a1bdc5c05455504b8e74f12c3edb05780f
KommissarAndersonVargas <andersomrobertomachadodevargas@gmail.com>
2023-12-26 16:44:47 -0300

    api quase completa

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -2,6 +2,7 @@
 using APICatalogo.Controllers.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace APICatalogo.Controllers
 {
@@ -29,5 +30,64 @@ namespace APICatalogo.Controllers
                 return produtos;
             
         }
+        [HttpGet("{id:int}", Name ="Obter Produto")]
+        public ActionResult<Produto> Get(int id)
+        {
+
+            var produto  = _context.Produtos.FirstOrDefault(p=> p.ProdutoId == id);
+
+            if (produto is null)
+            {
+                return NotFound("Id de produto não encontrado");
+            }
+
+            return produto;
+        }
+        [HttpPost]
+        public ActionResult Post (Produto produto)
+        {
+            if(produto is null)
+            {
+                return BadRequest();
+            }
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("Obter Produto",
+                new { id = produto.ProdutoId }, produto);
+        }
+
+
+        [HttpPut("{id:int}")]
+
+        public ActionResult Put (int id, Produto produto)
+        {
+
+            if(id != produto.ProdutoId)
+            {
+                return BadRequest("Produto não encontrado");
+            }
+            _context.Entry(produto).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(produto);
+        }
+        [HttpDelete("{id:int}")]
+
+        public ActionResult Delete(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(x => x.ProdutoId == id);
+            // procura o id por linq
+            if(produto is null)
+            {
+                return NotFound("Produto não localizado");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok(produto); // retorna o produto removido
+        }
+
     }
 }
